5c9b7fea8df27842b9f997be12a6886178b5734e
Jay-salot-2210 <jaykalpeshsalot@gmail.com>
2026-02-03 17:02:32 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/semantic-search': 'http://127.0.0.1:8001',
+      '/faculty': 'http://127.0.0.1:8001',
+      '/assets': 'http://127.0.0.1:8001',
+    }
+  }
 })
